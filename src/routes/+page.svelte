bb9c9f60d9a185b5d0de75668086ea8f31f372e5
kubiksonikdev <122747588+kubiksonikdev@users.noreply.github.com>
2023-06-06 22:05:12 +0200

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import CaseSection from '$components/errorCaseSection.svelte';
+  import CaseSection from '$components/errCaseSection.svelte';
   import type { CaseSection as CaseSectionType } from '$lib';
 
   export let data: { sections: CaseSectionType[] };
